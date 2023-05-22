c0143dab7c6d55c436d930de318a95f765b8ff76
MaragathaAtchaya <maragathaatchaya.m@gmail.com>
2023-05-22 18:16:29 +0800

    End of Section 8:TypeScript

--- onlineshop/Client/src/app/app.component.ts
+++ onlineshop/Client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Home';
+  products: Product[]=[];
+
+constructor (private http: HttpClient)  {}
+
+ngOnInit(): void {
+  this.http.get<Pagination<Product[]>>('https://localhost:5001/onlineshop/products?pagesize=30').subscribe({
+    next: Response=> this.products=Response.data, // what to do next
+    error: error=>console.log(error), //what to do if any error occurs
+    complete: ()=>{
+      console.log('Request completed'),
+      console.log('extra stmt')
+    }
+
+  })
+}
+
 }

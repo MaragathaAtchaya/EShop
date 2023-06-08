4fc1b5aba7fee2fbbdeaf19dd116882ed7e6eaaf
MaragathaAtchaya <maragathaatchaya.m@gmail.com>
2023-06-08 20:42:40 +0800

    End of Section 11 - Angular error handling

--- onlineshop/Client/src/app/core/core.module.ts
+++ onlineshop/Client/src/app/core/core.module.ts
@@ -2,16 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { TestErrorComponent } from './test-error/test-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { RouterModule } from '@angular/router';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    TestErrorComponent,
+    NotFoundComponent,
+    ServerErrorComponent
   ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
   ],
   exports:[
     NavBarComponent
